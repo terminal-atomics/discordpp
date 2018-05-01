1b0a4a14432f50d914d9323e0dc63bcdcc6cb8d1
Extragornax <gaspardwi@yahoo.fr>
2018-05-01 14:35:39 +0200

    Make travis logs more readable

--- install-all-deps.sh
+++ install-all-deps.sh
@@ -2,9 +2,9 @@
 
 if ! sudo apt-get update || ! sudo apt-get install -y libc++-dev libcurl4-openssl-dev; then
 	echo "Could not install with APT"
-	./tools/compile-libcurl.sh
-	./tools/install-json.sh
-	./tools/install-websocketpp.sh
+	./tools/compile-libcurl.sh > log_output
+	./tools/install-json.sh >> log_output
+	./tools/install-websocketpp.sh >> log_output
 else
 	echo "Updated with APT"
 fi
