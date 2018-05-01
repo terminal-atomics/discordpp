2ff2eb491815241ba9768839672a43d3f7a3ce0c
Extragornax <gaspardwi@yahoo.fr>
2018-05-01 14:37:55 +0200

    without sudo

--- install-all-deps.sh
+++ install-all-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! sudo apt-get update || ! sudo apt-get install -y libc++-dev libcurl4-openssl-dev; then
+if ! apt-get update || ! apt-get install -y libc++-dev libcurl4-openssl-dev; then
 	echo "Could not install with APT"
 	./tools/compile-libcurl.sh > log_output
 	./tools/install-json.sh >> log_output
