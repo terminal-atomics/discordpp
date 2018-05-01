3b9dc4d406bfe6d25db0f55a39e6c5941f601319
ahoZiorce <aho@sior.be>
2018-05-01 21:28:36 +0200

    Finished first version of discord_http (#4)

--- src/comp.sh
+++ src/comp.sh
@@ -1,3 +1,4 @@
 g++ -c main.cpp -o main.o -I../include --std=c++11 $(pkg-config --cflags openssl)
 g++ -c urler.cpp -o urler.o -I../include --std=c++11 -Werror $(pkg-config --cflags openssl)
-g++ main.o urler.o -o l -Wl,-Bstatic -lboost_thread -lboost_system -Wl,-Bdynamic -lpthread $(pkg-config --libs openssl)
+g++ -c discord_http.cpp -o discord_http.o -I../include --std=c++11 -Werror $(pkg-config --cflags openssl)
+g++ main.o urler.o discord_http.o -o l -Wl,-Bstatic -lboost_thread -lboost_system -Wl,-Bdynamic -lpthread $(pkg-config --libs openssl)
