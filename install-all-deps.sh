7063e0bb893945beb867cd39adc9b0330642b3fe
ahoZiorce <aho@sior.be>
2018-05-02 14:03:08 +0200

    Corrected unused dependencies in install-all-deps.sh

--- install-all-deps.sh
+++ install-all-deps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if ! sudo apt-get update || ! sudo apt-get install -y libc++-dev libcurl4-openssl-dev; then
+./tools/install-json.sh >> log_output
+./tools/install-websocketpp.sh >> log_output
+
+if ! sudo apt-get update || ! sudo apt-get install -y libc++-dev libssl-dev libboost-all-dev; then
 	echo "Could not install with APT"
-	./tools/compile-libcurl.sh > log_output
-	./tools/install-json.sh >> log_output
-	./tools/install-websocketpp.sh >> log_output
 else
 	echo "Updated with APT"
 fi
